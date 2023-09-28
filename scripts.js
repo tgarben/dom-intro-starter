126ed2ac010d8cc18c8731f903237b4e6e381df7
Tyler Garben <tylergarben@gmail.com>
2023-09-28 14:06:21 -0700

    Finished Assignment

--- scripts.js
+++ scripts.js
@@ -19,3 +19,20 @@ for(let span of ltBlueSpans){
 for(let span of medBlueSpans){
     span.style.color = "#3C5E73"
 }
+
+let colors = ["#283040","#3C5E73","#7C9EA6","#D9BCA3","#F2DCC9"]
+
+let svgs = document.querySelectorAll("#svgs svg")
+console.log(svgs);
+
+for(let i = 0; i < svgs.length; i++){
+    svgs[i].style.stroke = colors[i];
+}
+
+document.querySelector(".bold").style.color = "#7C9EA6";
+
+document.querySelector(".content_list li:first-of-type").textContent = "this is new item text";
+
+document.querySelectorAll(".content_list li")[2].innerHTML += " <strong>Tyler Garben</strong>";
+
+document.querySelector("#remove a").removeAttribute("hidden");
